f9afe9e11d5ae218b4246cd98ca7e89ec887f281
Mim-Hakkani <mimhakkani4g@gmail.com>
2023-03-06 07:30:43 +0600

    counter application is complety again

--- src/counter/Counter.jsx
+++ src/counter/Counter.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { counterAction, counterRandom } from '../redux/actions/counterActions';
+import { DEC, INC, OSTIRRESET, RAND, RESET } from '../redux/actionTypes/actionTypes';
 
 const Counter = () => {
-	return (
+
+	const {counter} = useSelector((state)=>state)
+	const dispatch = useDispatch()
+ 	return (
 		<div className='mt-10'>
 			<div className='card w-1/2 bg-base-100 shadow-xl m-auto'>
 				<div className='card-body'>
-					<h2 className='text-center text-5xl mb-3'> 0 </h2>
+					<h2 className='text-center text-5xl mb-3'> {counter} </h2>
 					
-					<div className='card-actions justify-end'>
-						<button className='btn btn-active'>Increment</button>
-						<button className='btn btn-active btn-primary'>Decrement</button>
-						<button className='btn btn-active btn-secondary'>Reset</button>
-						<button className='btn btn-active btn-accent'>Osthr Reset</button>
-						<button className='btn btn-active btn-ghost'>Random</button>
+					<div className='card-actions justify-center'>
+						<button className='btn btn-active' onClick={()=>dispatch(counterAction(INC))}>Increment</button>
+						<button className='btn btn-active btn-primary' onClick={()=>dispatch(counterAction(DEC))}>Decrement</button>
+						<button className='btn btn-active btn-secondary' onClick={()=>dispatch(counterAction(RESET))}>Reset</button>
+						<button className='btn btn-active btn-accent' onClick={()=>dispatch(counterAction(OSTIRRESET))}>Osthr Reset</button>
+						<button className='btn btn-active btn-ghost' onClick={()=>dispatch(counterRandom(RAND,Math.floor(Math.random()*1000)))}>Random</button>
 					</div>
 				</div>
 			</div>
